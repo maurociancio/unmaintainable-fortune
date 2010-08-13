60c0bf5a84da7e3b643ab26776d767a65ed2092d
Mauro Ciancio <maurociancio@gmail.com>
2010-08-12 22:07:08 -0300

    remove comments

--- unmaintainable-fortune.py
+++ unmaintainable-fortune.py
@@ -5,14 +5,6 @@ import gtk
 
 class HelloWorld:
     def delete_event(self, widget, event, data=None):
-        # If you return FALSE in the "delete_event" signal handler,
-        # GTK will emit the "destroy" signal. Returning TRUE means
-        # you don't want the window to be destroyed.
-        # This is useful for popping up 'are you sure you want to quit?'
-        # type dialogs.
-
-        # Change FALSE to TRUE and the main window will not be destroyed
-        # with a "delete_event".
         return False
 
     # Another callback
@@ -24,16 +16,7 @@ class HelloWorld:
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_default_size(520, 240)
     
-        # When the window is given the "delete_event" signal (this is given
-        # by the window manager, usually by the "close" option, or on the
-        # titlebar), we ask it to call the delete_event () function
-        # as defined above. The data passed to the callback
-        # function is NULL and is ignored in the callback function.
         self.window.connect("delete_event", self.delete_event)
-    
-        # Here we connect the "destroy" event to a signal handler.  
-        # This event occurs when we call gtk_widget_destroy() on the window,
-        # or if we return FALSE in the "delete_event" callback.
         self.window.connect("destroy", self.destroy)
     
         # Sets the border width of the window.
